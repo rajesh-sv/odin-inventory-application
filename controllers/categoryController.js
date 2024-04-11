885648eb92720b73a225ca96e51798f81ad37d1e
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2024-04-11 13:20:51 +0530

    Define controller logic and add appropriate views

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,35 +2,149 @@ const { body, validationResult } = require("express-validator")
 const asyncHandler = require("express-async-handler")
 
 const Category = require("../models/category")
+const Item = require("../models/item")
 
 exports.categoryList = asyncHandler(async (req, res, next) => {
-  res.send("categoryList")
+  const categoryList = await Category.find().sort({ name: 1 }).exec()
+  res.render("categoryList", {
+    title: "Categories",
+    categoryList,
+  })
 })
 
 exports.createCategoryGet = asyncHandler(async (req, res, next) => {
-  res.send("createCategoryGet")
+  res.render("categoryForm", {
+    title: "Create Category",
+    category: {},
+  })
 })
 
-exports.createCategoryPost = asyncHandler(async (req, res, next) => {
-  res.send("createCategoryPost")
-})
+exports.createCategoryPost = [
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must be specified").trim().escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    })
+
+    if (errors.isEmpty()) {
+      await category.save()
+      res.redirect(category.url)
+    } else {
+      res.render("categoryForm", {
+        title: "Create Category",
+        category,
+        errors: errors.array(),
+      })
+    }
+  }),
+]
 
 exports.deleteCategoryGet = asyncHandler(async (req, res, next) => {
-  res.send("deleteCategoryGet")
+  const [category, allItemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find(
+      { category: req.params.id, number_in_stock: { $gt: 0 } },
+      "name description"
+    )
+      .sort({ name: 1 })
+      .exec(),
+  ])
+
+  if (category == null) {
+    res.redirect("/catalog/categories")
+  } else {
+    res.render("categoryDelete", {
+      title: "Delete Category",
+      category,
+      items: allItemsInCategory,
+    })
+  }
 })
 
 exports.deleteCategoryPost = asyncHandler(async (req, res, next) => {
-  res.send("deleteCategoryPost")
+  const [category, allItemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find(
+      { category: req.params.id, number_in_stock: { $gt: 0 } },
+      "name description"
+    )
+      .sort({ name: 1 })
+      .exec(),
+  ])
+
+  if (allItemsInCategory.length) {
+    res.render("categoryDelete", {
+      title: "Delete Category",
+      category,
+      items: allItemsInCategory,
+    })
+  } else {
+    await category.deleteOne().exec()
+    await Promise.all(allItemsInCategory.map((item) => item.deleteOne().exec()))
+    res.redirect("/catalog/categories")
+  }
 })
 
 exports.updateCategoryGet = asyncHandler(async (req, res, next) => {
-  res.send("updateCategoryGet")
+  const category = await Category.findById(req.params.id).exec()
+  if (category == null) {
+    res.redirect("/catalog/categories")
+  } else {
+    res.render("categoryForm", {
+      title: "Update Category",
+      category,
+    })
+  }
 })
 
-exports.updateCategoryPost = asyncHandler(async (req, res, next) => {
-  res.send("updateCategoryPost")
-})
+exports.updateCategoryPost = [
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must be specified").trim().escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+      _id: req.params.id,
+    })
+
+    if (errors.isEmpty()) {
+      const updatedCategory = await Category.findByIdAndUpdate(
+        req.params.id,
+        category
+      ).exec()
+      res.redirect(updatedCategory.url)
+    } else {
+      res.render("categoryForm", {
+        title: "Update Category",
+        category,
+        errors: errors.array(),
+      })
+    }
+  }),
+]
 
 exports.categoryDetail = asyncHandler(async (req, res, next) => {
-  res.send("categoryDetail")
+  const [category, allItemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }, "name description")
+      .sort({ name: 1 })
+      .exec(),
+  ])
+
+  if (category == null) {
+    const err = new Error("Category not found")
+    err.status = 404
+    return next(err)
+  } else {
+    res.render("categoryDetail", {
+      category,
+      allItemsInCategory,
+    })
+  }
 })
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -5,8 +5,10 @@ const Item = require("../models/item")
 const Category = require("../models/category")
 
 exports.index = asyncHandler(async (req, res, next) => {
-  const categoryCount = await Category.countDocuments().exec()
-  const itemCount = await Item.countDocuments().exec()
+  const [categoryCount, itemCount] = await Promise.all([
+    Category.countDocuments().exec(),
+    Item.countDocuments().exec(),
+  ])
 
   res.render("index", {
     title: "Intergalactic Spaceship Store",
@@ -16,33 +18,179 @@ exports.index = asyncHandler(async (req, res, next) => {
 })
 
 exports.itemList = asyncHandler(async (req, res, next) => {
-  res.send("itemList")
+  const itemList = await Item.find({}, "name category")
+    .populate("category")
+    .sort({ name: 1 })
+    .exec()
+  res.render("itemList", {
+    title: "Spaceships",
+    itemList,
+  })
 })
 
 exports.createItemGet = asyncHandler(async (req, res, next) => {
-  res.send("createItemGet")
+  const categories = await Category.find({}, "name").sort({ name: 1 }).exec()
+
+  res.render("itemForm", {
+    title: "Create Spaceship",
+    categories,
+  })
 })
 
-exports.createItemPost = asyncHandler(async (req, res, next) => {
-  res.send("createItemPost")
-})
+exports.createItemPost = [
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category", "Category must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price must be specified").trim().isNumeric().escape(),
+  body("number_in_stock", "Number in-stock must be specified")
+    .trim()
+    .isNumeric()
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+    })
+
+    if (errors.isEmpty()) {
+      await item.save()
+      res.redirect(item.url)
+    } else {
+      const categories = await Category.find({}, "name")
+        .sort({ name: 1 })
+        .exec()
+
+      res.render("itemForm", {
+        title: "Create Spaceship",
+        item,
+        categories,
+      })
+    }
+  }),
+]
 
 exports.deleteItemGet = asyncHandler(async (req, res, next) => {
-  res.send("deleteItemGet")
+  const item = await Item.findById(req.params.id, "name").exec()
+
+  if (item == null) {
+    res.redirect("/catalog/items")
+  } else {
+    res.render("itemDelete", {
+      title: "Delete Shaceship",
+      item,
+    })
+  }
 })
 
-exports.deleteItemPost = asyncHandler(async (req, res, next) => {
-  res.send("deleteItemPost")
-})
+exports.deleteItemPost = [
+  body("secretkey").trim().equals("42").escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+    const item = { name: req.body.itemname }
+
+    if (!errors.isEmpty()) {
+      res.render("itemDelete", {
+        title: "Delete Shaceship",
+        item,
+        secretkey: req.body.secretkey,
+      })
+    } else {
+      const item = await Item.findById(req.params.id).exec()
+      if (item.number_in_stock > 1) {
+        const newItem = new Item({
+          ...item,
+          number_in_stock: item.number_in_stock - 1,
+          _id: item._id,
+        })
+        await Item.findByIdAndUpdate(req.params.id, newItem).exec()
+      } else {
+        await Item.findByIdAndDelete(req.params.id).exec()
+      }
+      res.redirect("/catalog/items")
+    }
+  }),
+]
 
 exports.updateItemGet = asyncHandler(async (req, res, next) => {
-  res.send("updateItemGet")
+  const [categories, item] = await Promise.all([
+    Category.find({}, "name").sort({ name: 1 }).exec(),
+    Item.findById(req.params.id).exec(),
+  ])
+
+  res.render("itemForm", {
+    title: "Create Spaceship",
+    item,
+    categories,
+  })
 })
 
-exports.updateItemPost = asyncHandler(async (req, res, next) => {
-  res.send("updateItemPost")
-})
+exports.updateItemPost = [
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category", "Category must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price must be specified").trim().isNumeric().escape(),
+  body("number_in_stock", "Number in-stock must be specified")
+    .trim()
+    .isNumeric()
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+      _id: req.params.id,
+    })
+
+    if (errors.isEmpty()) {
+      const updatedItem = await Item.findByIdAndUpdate(
+        req.params.id,
+        item
+      ).exec()
+      res.redirect(updatedItem.url)
+    } else {
+      const categories = await Category.find({}, "name")
+        .sort({ name: 1 })
+        .exec()
+
+      res.render("itemForm", {
+        title: "Create Spaceship",
+        item,
+        categories,
+      })
+    }
+  }),
+]
 
 exports.itemDetail = asyncHandler(async (req, res, next) => {
-  res.send("itemDetail")
+  const item = await Item.findById(req.params.id).populate("category").exec()
+  if (item == null) {
+    const err = new Error("Spaceship not found")
+    err.status = 404
+    return next(err)
+  } else {
+    res.render("itemDetail", {
+      item,
+    })
+  }
 })
